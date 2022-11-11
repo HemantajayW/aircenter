c4857be00d2ed5e2b2c99872896f2e557122f4bc
HemantajayW <87561916+HemantajayW@users.noreply.github.com>
2022-11-11 23:04:28 +0530

    Add files via upload

---
+++ bonus_assignment.py
@@ -0,0 +1,36 @@
+
+file = open("C:/Users/heman_6e6o1cr/Downloads/sample-2mb-text-file.txt", "r+")
+frequent_word = ""
+frequency = 0
+lines = file.readlines()
+words = []
+dictionary = {}
+
+
+for line in lines:
+
+    line_word = line.lower().replace(',', '').replace('.', '').split(" ")
+
+    for w in line_word:
+
+        if len(w)>1 and w[-1] == '.':
+            w = w[0:len(w) - 1]
+
+        if w in dictionary:
+            dictionary[w] += 1
+        else:
+            dictionary.update({w: 1})
+
+
+print(dictionary[max(dictionary,key=dictionary.get)])
+print("Frequency: ", str(frequency))
+x=max(dictionary,key=dictionary.get)
+y="loopr"
+
+with open(r"C:/Users/heman_6e6o1cr/Downloads/sample-2mb-text-file.txt", 'r') as file:
+    data = file.read()
+    data = data.replace(x, y)
+with open(r"C:/Users/heman_6e6o1cr/Downloads/sample-2mb-text-file.txt", 'w') as file:
+
+    file.write(data)
+file.close()
